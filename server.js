c0f25ebadc37c7fe6e030ae399d9312e2602f52a
nahidslam799 <nahidslam799@gmail.com>
2026-07-08 22:08:12 +0600

    Set up Express server with Telegram bot integration

--- server.js
+++ server.js
@@ -1,1 +1,39 @@
+require("dotenv").config();
 
+const express = require("express");
+const TelegramBot = require("node-telegram-bot-api");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, {
+  polling: true,
+});
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(
+    msg.chat.id,
+    `👋 Welcome to SureSMS
+
+🆔 Your Login UID
+
+${msg.chat.id}
+
+এই UID Website-এ লিখে Login করুন।
+
+তারপর "Send OTP" চাপুন।`
+  );
+});
+
+app.get("/", (req, res) => {
+  res.send("SureSMS Bot Running Successfully ✅");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server Running on ${PORT}`);
+});
